cc4449a62e8479ebd27eecc8ff0ca045ef991fd7
HaeChanBong <haechan.bong@gmail.com>
2017-05-16 09:26:40 -0400

    icons
    vol. ctrl. & fb

--- js/proshno.js
+++ js/proshno.js
@@ -7,3 +7,17 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+var aud = document.getElementById("bgsong");
+var icon = document.getElementById("icon");
+function mute() {
+	
+	if(aud.muted == true) {
+		aud.muted = false;
+		icon.className = "glyphicon glyphicon-volume-up";
+	}
+	else {
+		aud.muted = true;
+		icon.className = "glyphicon glyphicon-volume-off";
+	}
+}
